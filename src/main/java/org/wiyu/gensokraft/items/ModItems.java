86fcfe96a6cce6a8db93ab2349eb7a72190ef4dd
wiyus <wiushisushi@gmail.com>
2025-02-15 19:13:24 +0100

    Set placeholder for Homing Amulet projectile

--- src/main/java/org/wiyu/gensokraft/items/ModItems.java
+++ src/main/java/org/wiyu/gensokraft/items/ModItems.java
@@ -5,13 +5,16 @@ import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+import org.wiyu.gensokraft.items.danmaku.*;
+
 import java.util.function.Function;
 
 public final class ModItems {
     private ModItems() {
     }
 
-    public static final Item PLACEHOLDER_SPELL = register("placeholder_spell", Item::new, new Item.Settings());
+    public static final Item HOMING_AMULET = register("homing_amulet", HomingAmulet::new, new Item.Settings().maxCount(1).rarity(Rarity.EPIC));
 
     private static Item register(String p, Function<Item.Settings, Item> i, Item.Settings s) {
         final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("gensokraft", p));
--- src/main/java/org/wiyu/gensokraft/items/danmaku/HomingAmulet.java
+++ src/main/java/org/wiyu/gensokraft/items/danmaku/HomingAmulet.java
@@ -1,4 +1,51 @@
 package org.wiyu.gensokraft.items.danmaku;
 
-public class HomingAmulet {
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.passive.CowEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+
+
+//Individual spell items are to be deprecated for a much more organized
+//"wand" item per friend
+public class HomingAmulet extends Item {
+    public HomingAmulet(Settings s) {
+        super(s);
+    }
+
+    @Override
+    public ActionResult use(World w, PlayerEntity u, Hand h) {
+        boolean[] exploded = {false};
+        u.sendMessage(Text.of("Homing Amulet was succesfully cast."), true);
+        CowEntity cow = EntityType.COW.create(w, SpawnReason.MOB_SUMMONED); //placeholder projectile
+        if (cow != null) {
+            cow.refreshPositionAndAngles(u.getBlockPos(), 0.0F, 0.0F);
+
+            cow.setVelocity(u.getRotationVector().x*3, u.getRotationVector().y, u.getRotationVector().z*3);
+
+            cow.setCustomName(Text.of("Homing Cow Amulet"));
+            cow.setCustomNameVisible(true);
+            w.spawnEntity(cow);
+            u.playSound(SoundEvents.ENTITY_BLAZE_SHOOT);
+
+            ServerTickEvents.START_SERVER_TICK.register(server -> {
+                if (!cow.isOnGround()) {
+                    return;
+                } else if (!exploded[0]){
+                    w.createExplosion(null, cow.getBlockX(), cow.getBlockY(), cow.getBlockZ(), 4.0F, World.ExplosionSourceType.MOB);
+                    cow.remove(Entity.RemovalReason.DISCARDED);
+                    exploded[0]=true;
+                }
+            });
+        }
+        return ActionResult.SUCCESS;
+    }
 }
